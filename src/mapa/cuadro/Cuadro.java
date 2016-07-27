a81a3cfa7362b58a234b851417b28e81a0118292
Neimolon <diegomatos1983@gmail.com>
2016-07-27 20:23:59 +0000

    Revert "23 - Graficos Mejorados"
    This reverts commit cd6cfd9a76cf7debef31bbdf34c90dda4778278b.

--- src/mapa/cuadro/Cuadro.java
+++ src/mapa/cuadro/Cuadro.java
@@ -25,10 +25,11 @@ public abstract class Cuadro {
         this.sprite = sprite;
         
     }
-
-    public void mostrar(int x,int y, Pantalla pantalla){
-        pantalla.mostrarCuadro(x << 5, y << 5, this);
-    }  
+    
+    public void mostrar(int x, int y, Pantalla pantalla){
+    
+    }
+    
     public boolean solido(){
         return false;
     }
--- src/mapa/cuadro/CuadroAsfalto.java
+++ src/mapa/cuadro/CuadroAsfalto.java
@@ -5,6 +5,7 @@ package mapa.cuadro;
     en el metodo heredado mostrar(x,y,pantalla):
         llama al metodo mostrar de pantalla pasando this para que los pixeles del sprite de este cuadro se copien a la pantalla
 */
+import graficos.Pantalla;
 import graficos.Sprite;
 
 public class CuadroAsfalto extends Cuadro{
@@ -12,5 +13,10 @@ public class CuadroAsfalto extends Cuadro{
     public CuadroAsfalto(Sprite sprite) {
         super(sprite);
     }
+
+    public void mostrar(int x,int y, Pantalla pantalla){
+        pantalla.mostrarCuadro(x, y, this);
+    }    
+
     
 }
